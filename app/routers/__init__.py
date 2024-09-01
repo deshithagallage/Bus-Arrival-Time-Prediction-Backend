3731dbbbc4f34762070927912365fabc189b98a0
Deshitha Gallage <deshithagallage623@gmail.com>
2024-09-01 22:36:22 +0530

    Lacally deploy the model

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from . import bus_route, bus_stop
+from . import bus_route, bus_stop, predict
 
 router = APIRouter()
 
 router.include_router(bus_route.router, prefix="/api/bus_routes", tags=["Bus Routes"])
 router.include_router(bus_stop.router, prefix="/api/bus_stops", tags=["Bus Stops"])
+router.include_router(predict.router, prefix="/api/model", tags=["Predictions"])
