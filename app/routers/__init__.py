5c2d8823cda8a09b7aeed120011ef2678cd20f86
Deshitha Gallage <deshithagallage623@gmail.com>
2024-10-05 13:20:17 +0530

    Create router for get hourly stats

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from . import bus_route, bus_stop, predict
+from . import bus_route, bus_stop, predict, hourly_stats
 
 router = APIRouter()
 
 router.include_router(bus_route.router, prefix="/api/bus_routes", tags=["Bus Routes"])
 router.include_router(bus_stop.router, prefix="/api/bus_stops", tags=["Bus Stops"])
 router.include_router(predict.router, prefix="/api/model", tags=["Predictions"])
+router.include_router(hourly_stats.router, prefix="/api/hourly_stats", tags=["Hourly Stats"])
